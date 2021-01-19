42b092ae1283a3b133f24eec8e67cd8763519230
santugowda <meetsanthoshbl@gmail.com>
2021-01-18 19:16:34 -0800

    Implemented main UI screen
    1. Added assets
    2. Added fragment_equipment
    3. String used in app

--- app/src/main/java/com/example/catdigital_yardclub/ui/equipment/RequestEquipmentFragment.kt
+++ app/src/main/java/com/example/catdigital_yardclub/ui/equipment/RequestEquipmentFragment.kt
@@ -4,28 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.example.catdigital_yardclub.R
 
 class RequestEquipmentFragment : Fragment() {
-
-    private lateinit var requestEquipmentViewModel: RequestEquipmentViewModel
 
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        requestEquipmentViewModel =
-                ViewModelProviders.of(this).get(RequestEquipmentViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_equipment_request)
-        requestEquipmentViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
-        return root
+        return inflater.inflate(R.layout.fragment_equipment, container, false)
     }
 }
