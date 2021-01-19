cf6d30260a30bfd481c2c9c94673a9d9a916e8cd
santugowda <meetsanthoshbl@gmail.com>
2021-01-18 22:02:15 -0800

    Updated bottom navigation style

--- app/src/main/java/com/example/catdigital_yardclub/MainActivity.kt
+++ app/src/main/java/com/example/catdigital_yardclub/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.catdigital_yardclub
 
 import android.os.Bundle
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,8 +18,15 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.navigation_equipment, R.id.navigation_rental, R.id.navigation_requests, R.id.navigation_yard))
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_equipment,
+                R.id.navigation_rental,
+                R.id.navigation_requests,
+                R.id.navigation_yard
+            )
+        )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
